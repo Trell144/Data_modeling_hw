14a7145b80ffcc87c53b665cd70c2de5037910d2
Catrell Matthews <catrellmatthewss@gmail.com>
2018-09-18 13:31:42 -0400

    initial commit

---
+++ Data_modeling_hw.js
@@ -0,0 +1,38 @@
+App for ultra email
+
+A User has :
+- a name
+- an id
+- a phone number
+- a gender
+- a of number of email accounts
+
+A Email account has:
+- a service provider
+- a type
+- an address
+- a user name
+- a passwrod
+
+Emails:
+- a date
+- an account
+- a sender
+- a title/subject
+
+Ultra email js:
+
+let User = {
+    name: String,
+    id: Number,
+    phone: Number,
+    gender: String,
+    numberOfAccounts: Number
+}
+
+let emailAccounts = {
+    date: String,
+    account: String,
+    sender: String,
+    
+}
